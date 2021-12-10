82b77169f475f27cbb4ce978552a6e97b8fb1647
Steven Wells <Jskight@users.noreply.github.com>
2021-12-09 22:14:25 -0500

    Update challenge_3.sql

--- ORDER BY/Challenge 3/challenge_3.sql
+++ ORDER BY/Challenge 3/challenge_3.sql
@@ -3,9 +3,9 @@
  * @Date:   2021-12-09T11:57:39-05:00
  * @Email:  steven.wells1936@outlook.com
  * @Last modified by:   skigh
- * @Last modified time: 2021-12-09T12:09:44-05:00
+ * @Last modified time: 2021-12-09T22:14:17-05:00
  */
- 
+
  SELECT COUNT(length)
  FROM film
  WHERE length <= 50
